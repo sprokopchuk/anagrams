3e264052531d8cd5975b26a8046c089de4594e60
Sergey Prokopchuk <svp@anahoret.com>
2017-01-30 13:02:26 +0200

    Implement wordOccurrences function

--- src/main/scala/forcomp/Anagrams.scala
+++ src/main/scala/forcomp/Anagrams.scala
@@ -34,7 +34,9 @@ object Anagrams {
    *
    *  Note: you must use `groupBy` to implement this method!
    */
-  def wordOccurrences(w: Word): Occurrences = ???
+  def wordOccurrences(w: Word): Occurrences = {
+    w.toLowerCase.toList.groupBy(x => x).mapValues(x => x.length).toList.sortBy(_._1)
+  }
 
   /** Converts a sentence into its character occurrence list. */
   def sentenceOccurrences(s: Sentence): Occurrences = ???
